9bff6b2f466656d84a1c76eaa1d82d26f7f09ff1
Rafael Oliveira <rafaelhenrique.oliveira7@gmail.com>
2020-07-13 22:36:29 -0300

    first commit

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,59 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = service.insert(user);
+
+        URI uri = ServletUriComponentsBuilder.
+                fromCurrentRequestUri().
+                path("/{id}").
+                buildAndExpand(user.getId()).
+                toUri();
+
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = service.update(id, user);
+
+        return ResponseEntity.ok().body(user);
+    }
+}
